140b84f7b473c833eef0897d397611b4d0ada490
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:00:26 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase=Integer.parseInt(br.readLine());
+        int count=0;
+        while(testCase-->0){
+            boolean[] array = new boolean[26];
+            String s = br.readLine();
+            boolean tf = true;
+
+            array[s.charAt(0)-97]=true;
+            for (int i=1; i<s.length(); i++){
+                if(s.charAt(i-1)==s.charAt(i)) continue;
+                if(array[s.charAt(i)-97]){
+                    tf=false;
+                    break;
+                }
+                array[s.charAt(i)-97] = true;
+            }
+            if(tf) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+
+
+
+    }
+}
