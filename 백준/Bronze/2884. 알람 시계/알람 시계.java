72b967226f3c9eeedbd95079648e7cebdf29940a
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:08:35 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+
+        if(m>=45){
+            sb.append(h).append(" ").append(m-45);
+            bw.write(sb.toString());
+        }
+        else if (h==0) {
+            sb.append(23).append(" ").append(m+60-45);
+            bw.write(sb.toString());
+        }
+        else {
+            sb.append(h-1).append(" ").append(m+60-45);
+            bw.write(sb.toString());
+        }
+        bw.close();
+    }
+}
