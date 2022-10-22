25a27a336bda435ea49f05630912446a3e1c2a95
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:03:20 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 160 ms, Memory: 15540 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+        while(testCase-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int students = Integer.parseInt(st.nextToken());
+            int[] score = new int[students];
+            float average = 0;
+            for (int i=0; i<students; i++){
+                score[i] = Integer.parseInt(st.nextToken());
+                average += score[i];
+            }
+            average /= students;
+
+            float count=0;
+            for (int i=0; i<students; i++){
+                if(score[i]>average) count++;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("%.3f",count/students*100)).append("%\n");
+            bw.write(sb.toString());
+
+        }
+        bw.close();
+
+    }
+}
