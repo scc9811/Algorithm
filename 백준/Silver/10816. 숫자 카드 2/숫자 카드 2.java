382b5b4e75f9adcf5d27fbf403d268be53962a6b
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:05:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 892 ms, Memory: 197680 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int[] array = new int[20000001];
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int size = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (size-->0){
+            array[Integer.parseInt(st.nextToken())+10000000]++;
+        }
+
+        int testCase = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(testCase-->0){
+
+            sb.append(array[Integer.parseInt(st.nextToken())+10000000]).append(' ');
+        }
+        sb.delete(sb.length()-1,sb.length());
+        bw.write(sb.toString());
+        bw.close();
+
+    }
+
+}
