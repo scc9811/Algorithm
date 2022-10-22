cbc288d761da7a145091b93d17e3b877df75afcf
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:09:12 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 176 ms, Memory: 17300 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class Main {
+    static boolean[] array = new boolean[10001];
+    public static void main(String[] args)throws IOException {
+        array[2] = true;
+        for (int i=3; i<=10000; i++){
+            boolean tf = true;
+            for (int j=2; j*j<=i; j++){
+                if(i%j==0){
+                    tf = false;
+                    break;
+                }
+            }
+            if (tf) array[i] = true;
+            else array[i] = false;
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+
+        while (testCase-->0){
+            StringBuilder sb = new StringBuilder();
+            int n = Integer.parseInt(br.readLine());
+            for (int i=n/2; i<n; i++){
+                if (array[i] && array[n-i]){
+                    sb.append(n-i).append(" ").append(i).append("\n");
+                    bw.write(sb.toString());
+                    break;
+                }
+            }
+
+
+        }
+        bw.close();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
