f2201c53346b35eea9b8893e23c154466e45eb23
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:08:16 +0900

    [Bronze IV] Title: 주사위 세개, Time: 136 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int first = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
+        int third = Integer.parseInt(st.nextToken());
+
+        int result;
+        if(first==second && second==third){
+            result = 10000+first*1000;
+        }
+        else if (first!=second && second!=third && first!=third){
+            result = Math.max(Math.max(first,second),third)*100;
+        }
+        else if (first==second) result = 1000+first*100;
+        else result = 1000+third*100;
+
+        bw.write(String.valueOf(result));
+        bw.close();
+
+    }
+}
