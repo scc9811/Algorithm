59ed26b36a8be7926abe0cacae8d6121197fbea7
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-10-22 22:09:38 +0900

    [Silver III] Title: 베르트랑 공준, Time: 908 ms, Memory: 14688 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            int n = Integer.parseInt(br.readLine());
+            if (n==0) break;
+            int count = 0;
+            for (int i=n+1; i<=2*n; i++){
+                boolean tf = true;
+                for (int j=2; j*j<=i; j++){
+                    if(i%j==0) {
+                        tf = false;
+                        break;
+                    }
+                }
+                if (tf) count++;
+            }
+            System.out.println(count);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
