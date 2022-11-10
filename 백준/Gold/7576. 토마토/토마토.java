22347c3205625ee73efff789fbf2c5fe1c48d020
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-11-11 03:03:58 +0900

    [Gold V] Title: 토마토, Time: 668 ms, Memory: 120772 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,90 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[][] array = new int[1000][1000];
-    public static void main(String[] args)throws IOException{
-        Queue<Integer> queue = new LinkedList<>();
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int n, m, arr[][];
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-
-        int tmp = 0;
-        for (int i=0; i<N; i++){
+        Queue<Point>  queue = new LinkedList<>();
+        arr = new int[n][m];
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<M; j++){
-                array[i][j] = Integer.parseInt(st.nextToken());
-                if (array[i][j]==1){
-                    queue.add(i);
-                    queue.add(j);
-                    tmp++;
-                }
+            for(int j=0; j<m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j]==1) queue.add(new Point(i, j));
             }
         }
 
 
 
-        int i = 0;
-        int j = 0;
-        int day = 0;
-        while (!queue.isEmpty()){
-            i = queue.poll();
-            j = queue.poll();
-            if(i+1<N && array[i+1][j]==0){
-                array[i+1][j] = 1;
-                queue.add(i+1);
-                queue.add(j);
+        int days = 0;
+        int tmp = queue.size();
+        while(!queue.isEmpty()){
+            Point p = queue.poll();
+            int startI = p.x;
+            int startJ = p.y;
+            for(int i=0; i<4; i++){
+                int newI = startI+dx[i];
+                int newJ = startJ+dy[i];
+                if(newI>=0 && newI<n && newJ>=0 && newJ<m && arr[newI][newJ]==0){
+                    arr[newI][newJ] = 1;
+                    queue.add(new Point(newI, newJ));
+                }
             }
-            if(i-1>-1 && array[i-1][j]==0){
-                array[i-1][j] = 1;
-                queue.add(i-1);
-                queue.add(j);
-            }
-            if(j+1<M && array[i][j+1]==0){
-                array[i][j+1] = 1;
-                queue.add(i);
-                queue.add(j+1);
-            }
-            if (j-1>-1 && array[i][j-1]==0){
-                array[i][j-1] = 1;
-                queue.add(i);
-                queue.add(j-1);
-            }
+
             tmp--;
-            if (tmp==0){
-                day++;
-                tmp = queue.size()/2;
-            }
-
-
-//            for(int k=0; k<N; k++){
-//                System.out.println(Arrays.toString(array[k]));
-//            }
-//            System.out.println();
-
-        }
-        boolean tf = true;
-        for (i=0; i<N; i++){
-            for (j=0; j<M; j++){
-                if (array[i][j]==0){
-                    tf = false;
-                    break;
-                }
+            if(tmp==0){
+                days++;
+                tmp = queue.size();
             }
         }
-        if (tf) System.out.println(day-1);
-        else  System.out.println(-1);
+        days--;
+
+        if(TF()) System.out.println(days);
+        else System.out.println(-1);
 
 
 
 
 
     }
+    static class Point{
+        int x, y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static boolean TF(){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(arr[i][j]==0) return false;
+            }
+        }
+
+        return true;
+    }
+
 }
