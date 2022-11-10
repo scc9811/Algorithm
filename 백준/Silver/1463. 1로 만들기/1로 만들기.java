57643072f110a34ebd135b9d5faddad5d45512a1
scc9811 <101318479+scc9811@users.noreply.github.com>
2022-11-11 02:28:07 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,39 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner stdIn = new Scanner(System.in);
-        int n = stdIn.nextInt();
-        int[] dp = new int[n+1];
-        for (int i=2; i<dp.length; i++){
-            if(i%6==0){
-                dp[i] = Math.min(dp[i/2],dp[i/3])+1;
-            }
-            else if(i%3==0){
-                dp[i] = Math.min(dp[i/3],dp[i-1])+1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        arr[1] = 0;
+        if(n>1)arr[2] = 1;
+        if(n>2)arr[3] = 1;
+        for(int i=4; i<=n; i++){
+            int min;
+            if(i%2==0 && i%3==0){
+                min = Math.min(arr[i/2],arr[i/3]);
+                min = Math.min(min,arr[i-1]);
             }
             else if(i%2==0){
-                dp[i] = Math.min(dp[i/2],dp[i-1])+1;
+                min = Math.min(arr[i/2], arr[i-1]);
             }
-            else dp[i]=dp[i-1]+1;
+            else if(i%3==0){
+                min = Math.min(arr[i/3], arr[i-1]);
+            }
+            else{
+                min = arr[i-1];
+            }
 
+            arr[i] = min+1;
         }
-        System.out.println(dp[n]);
+        System.out.println(arr[n]);
+
+
+
+
+
     }
 }
