5c4debcb32621331b76acedeff584f8d309ea180
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-01-06 13:30:10 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int[] dx = {0,1,2,3,4,5,7,8};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
@@ -12,13 +13,11 @@ public class Main {
             arr[s.charAt(i)-'0']++;
         }
 
-
         int max = (arr[6]+arr[9]-1)/2 +1;
-
-        for(int i=0; i<10; i++){
-            if(i==6 || i==9) continue;
-            max = Math.max(max, arr[i]);
+        for(int i : dx){
+            if(arr[i]>max) max = arr[i];
         }
+
         System.out.println(max);
 
 
