84b09cae979f3fee6c2423e46a61a025658d7311
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-01-29 22:06:40 +0900

    [Silver V] Title: BABBA, Time: 136 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n+1][2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        for(int i=1; i<=n; i++){
+            dp[i][0] = dp[i-1][1];
+            dp[i][1] = dp[i-1][0] + dp[i-1][1];
+        }
+        System.out.printf("%d %d\n", dp[n][0], dp[n][1]);
+
+
+
+
+
+
+
+    }
+}
