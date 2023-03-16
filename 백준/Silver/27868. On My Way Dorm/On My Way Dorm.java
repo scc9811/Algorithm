90dbb88912c96ecae6b0cb937fb5f989f1c3b121
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-03-16 21:52:50 +0900

    [Silver III] Title: On My Way Dorm, Time: 336 ms, Memory: 25888 KB -BaekjoonHub

---
+++ 백준/Silver/27868. On My Way Dorm/On My Way Dorm.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.println(sb.reverse().toString());
+
+
+
+
+
+
+
+
+
+
+    }
+}
