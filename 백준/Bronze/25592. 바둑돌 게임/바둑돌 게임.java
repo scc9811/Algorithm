f00acf3fa945c3592c6ff4aa87b89b05dc9e9902
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-06-26 06:56:03 +0900

    [Bronze I] Title: 바둑돌 게임, Time: 128 ms, Memory: 14700 KB -BaekjoonHub

---
+++ 백준/Bronze/25592. 바둑돌 게임/바둑돌 게임.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        int sum = 0;
+        int step = 1;
+        while(sum<(int)1e8){
+            sum += step;
+            list.add(sum);
+            step++;
+        }
+
+        
+
+        int flag = 0;
+        int res = 0;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)>=n){
+                if(flag==0){
+                    res = list.get(i)-n;
+                }
+                else{
+                    if(n==list.get(i)) res = list.get(i+1)-n;
+                }
+                break;
+            }
+            flag = (flag+1)%2;
+        }
+
+        System.out.println(res);
+
+
+
+
+
+    }
+    final static int MOD = 1000000007;
+    static class SegTree {
+        long[] tree;
+        SegTree(int n){
+            long treeHeight = (long)Math.ceil(Math.log(n)/Math.log(2))+1;
+            long treeNodeCount = (long)Math.pow(2,treeHeight);
+            tree = new long[(int)treeNodeCount];
+        }
+        long init(long[] arr, int node, int start, int end){
+            if(start==end) return tree[node] = arr[start];
+            else return tree[node] = (init(arr, node*2, start, (start+end)/2) *
+                    init(arr, node*2+1, (start+end)/2+1, end))%MOD;
+        }
+        long mul(int node, int start, int end, int left, int right){
+            if(start>right || end<left) return 1;
+            else if(start<=left && end>=right) return tree[node];
+            else return (mul(node*2, start, end, left, (left+right)/2) *
+                        mul(node*2+1, start, end, (left+right)/2+1, right))%MOD;
+        }
+        long update(int node, int start, int end, int index, long diff){
+            if(start > index || end < index) return tree[node];
+            if(start==end) return tree[node] = diff;
+            return tree[node] = (update(node*2, start, (start+end)/2, index, diff)*
+                    update(node*2, (start+end)/2+1, end, index, diff));
+        }
+    }
+}
