1cbd9a8f9f73ad5e8f5b57ea3718b590a42d099c
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-10 23:08:52 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 660 ms, Memory: 63860 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,21 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    static int n, e;
+    static final int INF = (int)1e9 + 2;
+    static int n, m;
     static Node[] graph;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         graph = new Node[n+1];
-        for(int i=0; i<e; i++){
+        for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
@@ -27,63 +23,56 @@ public class Main{
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
 
+        int dis1 = dijkstra(1, v1);
+        int dis2 = dijkstra(v1, v2);
+        int dis3 = dijkstra(v2, n);
+        int res = -1;
 
-        // case1
-        int dis1 = Dijkstra(1, v1);
-        int dis2 = Dijkstra(v1, v2);
-        int dis3 = Dijkstra(v2, n);
-        int case1 = dis1+dis2+dis3;
-
-        dis1 = Dijkstra(1, v2);
-        dis2 = Dijkstra(v2, v1);
-        dis3 = Dijkstra(v1, n);
-        int case2 = dis1+dis2+dis3;
-        int res;
-
-        if(case1<0){
-            if(case2<0) res = -1;
-            else res = case2;
+        if(dis1!=INF && dis2!=INF && dis3!=INF){
+            res = dis1+dis2+dis3;
         }
-        else{
-            if(case2<0) res = case1;
-            else res = Math.min(case1, case2);
+
+        dis1 = dijkstra(1, v2);
+        dis2 = dijkstra(v2, v1);
+        dis3 = dijkstra(v1, n);
+        if(dis1!=INF && dis2!=INF && dis3!=INF){
+            if(res==-1) res=dis1+dis2+dis3;
+            else res = Math.min(res, dis1+dis2+dis3);
         }
+
         System.out.println(res);
 
 
 
-
-
-
-
-
-
-
-
     }
-    static int Dijkstra(int start, int end){
-        if(start==end) return 0;
-        int[] distance = new int[n+1];
-        Arrays.fill(distance, (int)1e9);
-        distance[start] = 0;
-        boolean[] visited = new boolean[n+1];
-        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(new Node(start, 0));
+    static int dijkstra(int start, int end){
+        int[] dis = new int[n+1];
+        Arrays.fill(dis, INF);
+        dis[start] = 0;
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
+        for(Node node=graph[start]; node!=null; node=node.next){
+            if(dis[node.to] > node.weight){
+                dis[node.to] = node.weight;
+                priorityQueue.add(new Edge(node.to, dis[node.to]));
+            }
+        }
+
         while(!priorityQueue.isEmpty()){
-            Node cur = priorityQueue.poll();
-            if(end==cur.to) return cur.weight;
-            if(visited[cur.to]) continue;
-            visited[cur.to] = true;
-            for(Node node=graph[cur.to]; node!=null; node=node.next) {
-                if (distance[node.to] > node.weight + cur.weight) {
-                    distance[node.to] = node.weight + cur.weight;
-                    priorityQueue.add(new Node(node.to, distance[node.to]));
+            Edge e = priorityQueue.poll();
+            if(e.weight > dis[e.to]) continue;
+            for(Node node=graph[e.to]; node!=null; node=node.next){
+                if(dis[node.to] > dis[e.to] + node.weight){
+                    dis[node.to] = dis[e.to] + node.weight;
+                    priorityQueue.add(new Edge(node.to, dis[node.to]));
                 }
             }
         }
-        return (int) -1e9/2;
+//        System.out.printf("%d, %d\n", start, end);
+//        System.out.println(Arrays.toString(dis));
+//        System.out.println();
+        return dis[end];
     }
-    static class Node implements Comparable<Node>{
+    static class Node{
         int to, weight;
         Node next;
         Node(int to, int weight, Node next){
@@ -91,13 +80,15 @@ public class Main{
             this.weight = weight;
             this.next = next;
         }
-        Node(int to, int weight){
+    }
+    static class Edge implements Comparable<Edge>{
+        int to, weight;
+        Edge(int to, int weight){
             this.to = to;
             this.weight = weight;
         }
-        public int compareTo(Node n){
-            return Integer.compare(this.weight, n.weight);
+        public int compareTo(Edge e){
+            return Integer.compare(this.weight, e.weight);
         }
-
     }
 }
