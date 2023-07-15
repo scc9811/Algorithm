a402bc1c53f5648afdd517beea5cbf43062678aa
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-15 23:10:10 +0900

    [Silver I] Title: 미로 탐색, Time: 180 ms, Memory: 16696 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,11 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 public class Main{
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,1,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,54 +13,46 @@ public class Main{
                 arr[i][j] = s.charAt(j)-'0';
             }
         }
-//        boolean[][] visited = new boolean[n][m];
-
         Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0,0));
+        queue.add(new Point(0, 0));
         arr[0][0] = 0;
-//        for(int i=0; i<n; i++){
-//            System.out.println(Arrays.toString(arr[i]));
-//        }
-
+        int count = 1;
         int tmp = 1;
-        int count = 0;
+
         while(!queue.isEmpty()){
             tmp--;
             if(tmp==0){
-                count++;
                 tmp = queue.size();
+                count++;
             }
             Point p = queue.poll();
-            int startI = p.x;
-            int startJ = p.y;
-//            System.out.println("startI = "+startI);
-//            System.out.println("startJ = "+startJ);
-//            System.out.println();
-            for(int i=0; i<4; i++){
-                int newI = startI+dx[i];
-                int newJ = startJ+dy[i];
+            for(int i =0; i<4; i++){
+                int newI = p.i+dx[i];
+                int newJ = p.j+dy[i];
                 if(newI>=0 && newI<n && newJ>=0 && newJ<m && arr[newI][newJ]==1){
-                    if(newI==n-1 && newJ==m-1){
-                        queue = new LinkedList<>();
-                        break;
-                    }
-                    arr[newI][newJ]=0;
                     queue.add(new Point(newI, newJ));
+                    if(newI==n-1 && newJ==m-1){
+                        System.out.println(count);
+                        System.exit(0);
+                    }
+                    arr[newI][newJ] = 0;
                 }
             }
         }
-        System.out.println(count+1);
+
 
 
 
 
 
     }
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
     static class Point{
-        int x, y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
+        int i, j;
+        Point(int i, int j){
+            this.i = i;
+            this.j = j;
         }
     }
 }
