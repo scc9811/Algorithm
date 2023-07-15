6b8e842393739912a150d6f7a2e17226439a8397
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-16 00:25:41 +0900

    [Gold V] Title: 집합의 표현, Time: 496 ms, Memory: 53456 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,8 +1,5 @@
-import javax.swing.text.Segment;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,7 +8,7 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         par = new int[n+1];
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<=n; i++){
             par[i] = i;
         }
         StringBuilder sb = new StringBuilder();
@@ -20,31 +17,29 @@ public class Main {
             int flag = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if(flag==0) union(a, b);
-            else {
-                if(find(a)==find(b)) sb.append("YES");
-                else sb.append("NO");
-                sb.append('\n');
+            if(flag==0){
+                union(a, b);
+            }
+            else{
+                int p_a = find(a);
+                int p_b = find(b);
+                sb.append(p_a==p_b ? "YES" : "NO").append('\n');
             }
         }
         System.out.println(sb);
 
 
-
-
-
-
-
-
     }
+
     static int[] par;
-    static int find(int a){
-        if(par[a]==a) return a;
+
+    static int find(int a) {
+        if (par[a] == a) return a;
         return par[a] = find(par[a]);
     }
     static void union(int a, int b){
         int p_a = find(a);
         int p_b = find(b);
-        if(p_a!=p_b) par[p_a] = p_b;
+        par[p_a] = p_b;
     }
 }
