6f0f104c115b094cad8a8a0f490ff84fccadb563
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-25 19:39:36 +0900

    [Silver I] Title: 안전 영역, Time: 368 ms, Memory: 56480 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,46 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-public class Main {
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static Queue<Integer> queue = new LinkedList<>();
-    static int[][] array;
-    static boolean[][] tfArray;
-    static int size;
-    public static void main(String[] args)throws IOException {
+import java.io.*;
+import java.util.*;
+public class Main{
+    static int n, arr[][];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        size = Integer.parseInt(br.readLine());
-        int max = 0;
-        array = new int[size][size];
-        tfArray = new boolean[size][size];
-        for(int i=0; i<size; i++){
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j<size; j++){
-                array[i][j] = Integer.parseInt(st.nextToken());
-                max = Math.max(max,array[i][j]);
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-
-        int safetyArea_max = 1;
-        for(int i=1; i<=max+1; i++){
-            for(int j=0; j<size; j++){
-                for(int k=0; k<size; k++){
-                    if(array[j][k]==i) array[j][k]=0;
-                }
-            }
-            safetyArea_max = Math.max(safetyArea_max,countArea(i));
+        int res = 1;
+        for(int height=1; height<=100; height++){
+            res = Math.max(res, bfs(height));
         }
-        System.out.println(safetyArea_max);
 
-
+        System.out.println(res);
 
 
 
@@ -48,47 +26,39 @@ public class Main {
 
 
     }
-    public static int countArea(int depth){
-        int count=0;
-        int startI, startJ;
-        while(true) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (!tfArray[i][j] && array[i][j] > 0) {
-                        queue.add(i);
-                        queue.add(j);
-                        tfArray[i][j]=true;
-                        break;
-                    }
-                }
-                if (!queue.isEmpty()) break;
-            }
-            if(queue.isEmpty()) break;
-
-            while (!queue.isEmpty()) {
-
-                startI = queue.poll();
-                startJ = queue.poll();
-                for (int i = 0; i < 4; i++) {
-                    int newI = startI + dx[i];
-                    int newJ = startJ + dy[i];
-                    if (newI >= 0 && newI < size && newJ >= 0 && newJ < size) {
-                        if(array[newI][newJ]==0 || tfArray[newI][newJ]) continue;
-                        queue.add(newI);
-                        queue.add(newJ);
-                        tfArray[newI][newJ]=true;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    static int bfs(int height){
+        boolean[][] visited = new boolean[n][n];
+        int count = 0;
+        Queue<Point> queue = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] > height && !visited[i][j]){
+                    count++;
+                    queue.add(new Point(i, j));
+                    visited[i][j] = true;
+                    while(!queue.isEmpty()){
+                        Point curP = queue.poll();
+                        for(int k=0; k<4; k++){
+                            int newI = curP.i + dx[k];
+                            int newJ = curP.j + dy[k];
+                            if(newI>=0 && newI<n && newJ>=0 && newJ<n && !visited[newI][newJ] && arr[newI][newJ] > height){
+                                queue.add(new Point(newI, newJ));
+                                visited[newI][newJ] = true;
+                            }
+                        }
                     }
                 }
             }
-            count++;
         }
-        for (int i = 0; i < size; i++) {
-            Arrays.fill(tfArray[i], false);
-        }
-
-
-
-
         return count;
+    }
+    static class Point{
+        int i, j;
+        Point(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
     }
 }
