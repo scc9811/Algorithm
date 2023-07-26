a95398fd79137732f0fc7504dd956ddbbe6f6021
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-27 01:48:03 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,43 +1,42 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String s = input.next();
-		int count = s.length();
-
-		for (int i=0; i <s.length(); i++) {
-			if (i>0) {
-				if(s.charAt(i)=='=') {
-					if(s.charAt(i-1)=='c' || 
-							s.charAt(i-1)=='s' || s.charAt(i-1)=='z') {
-						count--;
-						if(i>1) {
-							if(s.charAt(i-1)=='z' && s.charAt(i-2)=='d') {
-								count --;
-							}
-						}
-					}
-				}
-				if(s.charAt(i)=='-') {
-					if(s.charAt(i-1)=='c' || s.charAt(i-1)=='d') {
-						count--;
-					}
-				}
-				if(s.charAt(i)=='j') {
-					if(s.charAt(i-1)=='l'||s.charAt(i-1)=='n') {
-						count--;
-					}
-				}
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int count = s.length();
+        for (int i=0; i <s.length(); i++) {
+            if (i>0) {
+                if(s.charAt(i)=='=') {
+                    if(s.charAt(i-1)=='c' ||
+                            s.charAt(i-1)=='s' || s.charAt(i-1)=='z') {
+                        count--;
+                        if(i>1) {
+                            if(s.charAt(i-1)=='z' && s.charAt(i-2)=='d') {
+                                count --;
+                            }
+                        }
+                    }
+                }
+                if(s.charAt(i)=='-') {
+                    if(s.charAt(i-1)=='c' || s.charAt(i-1)=='d') {
+                        count--;
+                    }
+                }
+                if(s.charAt(i)=='j') {
+                    if(s.charAt(i-1)=='l'||s.charAt(i-1)=='n') {
+                        count--;
+                    }
+                }
+            }
+        }
 
 
-		System.out.println(count);
-		input.close();
-
-	}
+        System.out.println(count);
 
 
 
+
+    }
 }
