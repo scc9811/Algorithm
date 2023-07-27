66b7a8b2651e4b69faffd8b34760669db41c4c26
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-07-27 19:51:51 +0900

    [Gold IV] Title: RGB거리 2, Time: 132 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,11 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        // red, green, blue
+
         int[][] arr = new int[n][3];
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,24 +13,30 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        
+
 
         int[][] dp = new int[n][3];
         int res = (int)1e9;
         for(int i=0; i<3; i++){
-            if(i==0){
-                dp[1][0] = (int)1e9;
-                dp[1][1] = arr[0][0] + arr[1][1];
-                dp[1][2] = arr[0][0] + arr[1][2];
-            }
-            else if(i==1){
-                dp[1][0] = arr[0][1] + arr[1][0];
-                dp[1][1] = (int)1e9;
-                dp[1][2] = arr[0][1] + arr[1][2];
-            }
-            else {
-                dp[1][0] = arr[0][2] + arr[1][0];
-                dp[1][1] = arr[0][2] + arr[1][1];
-                dp[1][2] = (int)1e9;
+            switch (i){
+                case 0 ->{
+                    dp[1][0] = (int) 1e9;
+                    dp[1][1] = arr[0][0] + arr[1][1];
+                    dp[1][2] = arr[0][0] + arr[1][2];
+                    break;
+                }
+                case 1 ->{
+                    dp[1][0] = arr[0][1] + arr[1][0];
+                    dp[1][1] = (int) 1e9;
+                    dp[1][2] = arr[0][1] + arr[1][2];
+                    break;
+                }
+                case 2 ->{
+                    dp[1][0] = arr[0][2] + arr[1][0];
+                    dp[1][1] = arr[0][2] + arr[1][1];
+                    dp[1][2] = (int) 1e9;
+                }
             }
 
             for(int j=2; j<n; j++){
@@ -38,10 +44,11 @@ public class Main {
                 dp[j][1] = Math.min(dp[j-1][0], dp[j-1][2]) + arr[j][1];
                 dp[j][2] = Math.min(dp[j-1][0], dp[j-1][1]) + arr[j][2];
             }
-            
+
             if(i==0) res = Math.min(res, Math.min(dp[n-1][1], dp[n-1][2]));
             else if(i==1) res = Math.min(res, Math.min(dp[n-1][0], dp[n-1][2]));
             else res = Math.min(res, Math.min(dp[n-1][0], dp[n-1][1]));
+
         }
 
         System.out.println(res);
