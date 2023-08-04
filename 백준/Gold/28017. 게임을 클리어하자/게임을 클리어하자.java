bfe08d8838de44ba235655c84b8bdaa38320bd5c
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-08-04 19:55:50 +0900

    [Gold V] Title: 게임을 클리어하자, Time: 428 ms, Memory: 36992 KB -BaekjoonHub

--- 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.java
+++ 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.java
@@ -1,4 +1,3 @@
-import javax.swing.text.Segment;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -43,49 +42,5 @@ public class Main{
 
 
 
-
-    }
-    static class SegTree {
-        private long[] tree;
-
-        SegTree(int n) {
-            double treeHeight = Math.ceil(Math.log(n) / Math.log(2)) +1;
-            long treeNodeCount = Math.round(Math.pow(2, treeHeight));
-            tree = new long[Math.toIntExact(treeNodeCount)];
-        }
-        long init(long[] arr, int node, int start, int end){
-            if(start==end){
-                return tree[node] = arr[start];
-            }
-            else {
-                return tree[node] = init(arr, node*2, start, (start+end)/2)  +
-                        init(arr, node*2 +1, (start+end)/2 + 1, end);
-            }
-        }
-        long sum(int node, int start, int end, int left, int right){
-            if(end < left || right < start){
-                return 0;
-            }
-            else if(left <= start && end <= right){
-                return tree[node];
-            }
-            else{
-                return sum(node*2, start, (start + end) / 2, left, right) +
-                        sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-            }
-        }
-
-        void update(int node, int start, int end, int index, long diff){
-            if(index < start || end < index){
-                return;
-            }
-            tree[node] = tree[node] + diff;
-            if(start!=end){
-                update(node*2, start, (start + end)/2, index, diff);
-                update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
-
-
-            }
-        }
     }
 }
