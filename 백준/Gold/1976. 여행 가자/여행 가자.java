dffccf9bbd7179603d06ffea06c3fbee0140a5cf
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-11-09 19:58:26 +0900

    [Gold IV] Title: 여행 가자, Time: 248 ms, Memory: 20204 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,37 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    static int n, m, par[];
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
         par = new int[n+1];
         for(int i=1; i<=n; i++){
             par[i] = i;
         }
+
         for(int i=1; i<=n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=1; j<=n; j++){
-                if(Integer.parseInt(st.nextToken())==1) union(i, j);
+                if(st.nextToken().charAt(0) == '1'){
+                    union(i, j);
+                }
             }
         }
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int i=0; i<m; i++){
-            set.add(find(Integer.parseInt(st.nextToken())));
+        int group = find(Integer.parseInt(st.nextToken()));
+        for(int i=1; i<m; i++){
+            if(group != find(Integer.parseInt(st.nextToken()))){
+                System.out.println("NO");
+                System.exit(0);
+            }
         }
-        System.out.println(set.size()==1 ? "YES" : "NO");
+        System.out.println("YES");
+        System.exit(0);
+
+
+
+
+
 
 
     }
-
-    static int[] par;
-
-    static int find(int a) {
-        if (par[a] == a) return a;
-        return par[a] = find(par[a]);
+    static int find(int a){
+        if(par[a] != a) return par[a] = find(par[a]);
+        return a;
     }
     static void union(int a, int b){
         int p_a = find(a);
