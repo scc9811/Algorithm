60e0501ab68e3ad7a64d77602e7c4886e74f2c15
scc9811 <101318479+scc9811@users.noreply.github.com>
2024-02-01 17:43:48 +0900

    [Silver I] Title: 상어 키우기, Time: 704 ms, Memory: 50312 KB -BaekjoonHub

--- 백준/Silver/30892. 상어 키우기/상어 키우기.java
+++ 백준/Silver/30892. 상어 키우기/상어 키우기.java
@@ -1,95 +1,54 @@
-import java.io.*;
-import java.util.*;
-public class Main{
-    static int[] par;
-    public static void main(String[] args) throws IOException{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        long t = Long.parseLong(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        par = new int[n];
-        for(int i=0; i<n; i++){
-            par[i] = i;
-        }
 
-        while(k-->0){
-            int l = 0;
-            int r = n-1;
-            int mid;
-            int maxIdx = -1;
-            while(l<=r){
-                mid = (l+r)/2;
-                if(arr[mid] < t){
-                    maxIdx = Math.max(maxIdx, mid);
-                    l = mid+1;
+        long sum = t;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<n && k>0; i++){
+            if(arr[i] >= sum){
+                if(stack.isEmpty()){
+                    break;
                 }
-                else r = mid-1;
+                else{
+                    sum += stack.pop();
+                    k--;
+                    i--;
+                }
             }
-            if(maxIdx == -1){
-                break;
+            else{
+                stack.add(arr[i]);
             }
-            int p_i = find(maxIdx);
-            if(p_i == -1) break;
-
-            t += arr[p_i];
-            union(p_i, p_i-1);
         }
-        System.out.println(t);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-    static int find(int a){
-        if(a==-1) return -1;
-        if(par[a] == a) return a;
-        else return par[a] = find(par[a]);
-    }
-    static void union(int a, int b){
-        int p_a = find(a);
-        int p_b = find(b);
-        if(p_a > p_b) par[p_a] = p_b;
-        else par[p_b] = p_a;
-    }
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    static class Point{
-        int x, y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
+        while(!stack.isEmpty() && k > 0){
+            sum += stack.pop();
+            k--;
         }
-    }
-    static class Node{
-        int to;
-        Node next;
-        Node(int to, Node next){
-            this.to = to;
-            this.next = next;
-        }
-    }
-    static int gcd(int a, int b){
-        if(b==0) return a;
-        return gcd(b, a%b);
-    }
-    static int lcm(int x, int y) {
-        return (x * y) / gcd(x, y);
+
+        System.out.println(sum);
+
+
+
+
+
+
     }
 }
