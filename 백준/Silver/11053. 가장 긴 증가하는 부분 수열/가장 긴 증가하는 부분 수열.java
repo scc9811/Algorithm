846a832e1207c3ac8cc001e4405299fa574b2e5d
scc9811 <wjsrudtjr98@naver.com>
2024-09-20 09:12:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,47 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int index = 1;
-        arr[0] = Integer.parseInt(st.nextToken());
-        for(int i=1; i<n; i++){
-            int key = Integer.parseInt(st.nextToken());
-            if(key > arr[index-1]) {
-                arr[index++] = key;
-            }
-            else if(key < arr[index-1]){
-                for(int j=0; j<index; j++){
-                    if(key <= arr[j]) {
-                        arr[j] = key;
-                        break;
-                    }
-                }
-            }
-//            System.out.println(Arrays.toString(arr));
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-//        System.out.println(Arrays.toString(arr));
 
-        System.out.println(index);
+        int[] maxLen = new int[n];
+        maxLen[0] = 1;
+
+        for(int i=1; i<n; i++) {
+            int maxLength = 0;
+            for(int j=0; j<i; j++) {
+                if(arr[i] > arr[j]) maxLength = Math.max(maxLength, maxLen[j]);
+            }
+            maxLen[i] = maxLength + 1;
+        }
+
+        int res = 0;
+        for(int i=0; i<n; i++) {
+            res = Math.max(res, maxLen[i]);
+        }
+
+        System.out.println(res);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,4 +49,3 @@ public class Main{
 
     }
 }
-
