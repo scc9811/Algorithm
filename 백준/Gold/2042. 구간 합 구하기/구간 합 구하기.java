e733377804e62aef96316814df7b5b41bc3bdd8b
전경석 <wjsrudtjr98@naver.com>
2025-08-01 21:20:55 +0900

    [Gold I] Title: 구간 합 구하기, Time: 576 ms, Memory: 102036 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,39 +1,42 @@
-import javax.swing.text.Segment;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static long[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        SegTree segTree = new SegTree(n);
-        arr = new long[n+1];
-        for(int i=1; i<=n; i++){
+
+        long[] arr = new long[n+1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = Long.parseLong(br.readLine());
         }
-        segTree.init(arr, 1, 1, n);
-        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<m+k; i++){
+        SegTree segTree = new SegTree(n);
+        segTree.init(arr, 1, 1, n);
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m + k; i++) {
             st = new StringTokenizer(br.readLine());
             int flag = Integer.parseInt(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             long c = Long.parseLong(st.nextToken());
-            if(flag==1){
-                segTree.update(1, 1, n, (int)b, c-arr[(int)b]);
-                arr[(int)b] = c;
+
+            if (flag == 1) {
+                segTree.update(1, 1, n, b, c - arr[b]);
+                arr[b] = c;
             }
-            else{
-                sb.append(segTree.sum(1, 1, n, (int)b, (int)c)).append('\n');
+            else {
+                sb.append(segTree.sum(1, 1, n, b, (int) c));
+                sb.append('\n');
             }
         }
+
         System.out.println(sb);
+        
 
 
 
@@ -42,46 +45,35 @@ public class Main {
 
     }
     static class SegTree {
-        private long[] tree;
+        long[] tree;
 
         SegTree(int n) {
-            double treeHeight = Math.ceil(Math.log(n) / Math.log(2)) +1;
-            long treeNodeCount = Math.round(Math.pow(2, treeHeight));
-            tree = new long[Math.toIntExact(treeNodeCount)];
+            double h = Math.ceil(Math.log(n) / Math.log(2)) + 1;
+            int nodeCount = (int) Math.round(Math.pow(2, h));
+            tree = new long[nodeCount];
         }
-        long init(long[] arr, int node, int start, int end){
-            if(start==end){
-                return tree[node] = arr[start];
-            }
-            else {
-                return tree[node] = init(arr, node*2, start, (start+end)/2)  +
-                        init(arr, node*2 +1, (start+end)/2 + 1, end);
-            }
+
+        long init(long[] arr, int node, int start, int end) {
+            if (start == end) return tree[node] = arr[start];
+            return tree[node] = init(arr, node*2, start, (start+end)/2) +
+                    init(arr, node*2+1, (start+end)/2+1, end);
         }
-        long sum(int node, int start, int end, int left, int right){
-            if(end < left || right < start){
-                return 0;
-            }
-            else if(left <= start && end <= right){
-                return tree[node];
-            }
-            else{
-                return sum(node*2, start, (start + end) / 2, left, right) +
+
+        long sum(int node, int start, int end, int left, int right) {
+            if (end < left || right < start) return 0;
+            else if (left <= start && end <= right) return tree[node];
+            else return sum(node * 2, start, (start + end) / 2, left, right) +
                         sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-            }
         }
 
-        void update(int node, int start, int end, int index, long diff){
-            if(index < start || end < index){
-                return;
-            }
+        void update(int node, int start, int end, int index, long diff) {
+            if (index < start || end < index) return;
             tree[node] = tree[node] + diff;
-            if(start!=end){
-                update(node*2, start, (start + end)/2, index, diff);
-                update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
-
-
+            if (start != end) {
+                update(node*2, start, (start+end)/2, index, diff);
+                update(node*2+1, (start+end)/2+1, end, index, diff);
             }
+
         }
     }
 }
