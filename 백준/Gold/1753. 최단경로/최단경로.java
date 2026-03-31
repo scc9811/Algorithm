5289d24782d8ab25d30bc7e1812641f3767ee7e6
scc9811 <wjsrudtjr98@naver.com>
2026-03-31 13:24:39 +0900

    [Gold IV] Title: 최단경로, Time: 584 ms, Memory: 101952 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,70 +1,67 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static final int INF = (int)1e9 + 2;
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] dis = new int[n+1];
-
-        int start = Integer.parseInt(br.readLine());
-        Arrays.fill(dis, INF);
-        Node[] graph = new Node[n+1];
-        for(int i=0; i<m; i++){
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(br.readLine());
+        int[] weights = new int[v+1];
+        int MAX_VALUE = (int) 1e9;
+        Arrays.fill(weights, MAX_VALUE);
+        weights[k] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Node[] graph = new Node[v+1];
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
             graph[from] = new Node(to, weight, graph[from]);
         }
-        dis[start] = 0;
-        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
-        for(Node node=graph[start]; node!=null; node=node.next){
-            if(dis[node.to] > node.weight){
-                dis[node.to] = node.weight;
-                priorityQueue.add(new Edge(node.to, dis[node.to]));
+
+        for (Node node = graph[k]; node != null; node = node.next) {
+            if (weights[node.to] > node.weight) {
+                weights[node.to] = node.weight;
+                pq.add(new Node(node.to, node.weight, null));
             }
         }
 
-        while(!priorityQueue.isEmpty()){
-            Edge e = priorityQueue.poll();
-            if(e.weight > dis[e.to]) continue;
-            for(Node node=graph[e.to]; node!=null; node=node.next){
-                if(dis[node.to] > dis[e.to] + node.weight){
-                    dis[node.to] = dis[e.to] + node.weight;
-                    priorityQueue.add(new Edge(node.to, dis[node.to]));
+        while (!pq.isEmpty()) {
+            Node curN = pq.poll();
+            if (weights[curN.to] < curN.weight) continue;
+
+            for (Node node = graph[curN.to]; node != null; node = node.next) {
+                if (weights[node.to] > weights[curN.to] + node.weight) {
+                    weights[node.to] = weights[curN.to] + node.weight;
+                    pq.add(new Node(node.to, weights[node.to], null));
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=n; i++){
-            sb.append(dis[i]==INF ? "INF" : dis[i]).append('\n');
+        for (int i = 1; i <= v; i++) {
+            if (weights[i] == MAX_VALUE) sb.append("INF");
+            else sb.append(weights[i]);
+            sb.append('\n');
         }
         System.out.println(sb);
 
 
 
+
     }
-    static class Node{
+    static class Node implements Comparable<Node>{
         int to, weight;
         Node next;
-        Node(int to, int weight, Node next){
+        Node (int to, int weight, Node next) {
             this.to = to;
             this.weight = weight;
             this.next = next;
         }
-    }
-    static class Edge implements Comparable<Edge>{
-        int to, weight;
-        Edge(int to, int weight){
-            this.to = to;
-            this.weight = weight;
-        }
-        public int compareTo(Edge e){
-            return Integer.compare(this.weight, e.weight);
+        public int compareTo(Node n) {
+            return Integer.compare(this.weight, n.weight);
         }
     }
 }
